155aeb6f86df8732a8bc581210bac7f4c68eeabc
Samir Anand <Samir.Anand@macmill.com>
2023-02-03 15:18:42 +0530

    global search

--- src/main/java/com/mps/think/setup/controller/AddOrderController.java
+++ src/main/java/com/mps/think/setup/controller/AddOrderController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mps.think.setup.service.AddOrderService;
@@ -74,5 +76,11 @@ public class AddOrderController {
 	public ResponseEntity<?> getAllOrdersByOrderClassId(@PathVariable("ocIc") Integer ocIc) throws Exception {
 		return ResponseEntity.ok(addOrderService.getAllOrdersByOrderClassId(ocIc));
 	}
+	
+//	@GetMapping("/getAllOrdersFromSearch")
+//	public ResponseEntity<?> getAllOrdersFromSearch(@RequestParam(required = false) String keyword, @RequestParam(defaultValue = "0") Integer page,
+//			@RequestParam(defaultValue = "5") Integer size) {
+//		return ResponseEntity.ok(addOrderService.getAllOrderFromSearch(keyword, PageRequest.of(page, size)));
+//	}
 
 }
---
+++ src/main/java/com/mps/think/setup/serviceImpl/GlobalSearchServiceImpl.java
@@ -0,0 +1,40 @@
+
+package com.mps.think.setup.serviceImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.mps.think.setup.model.CustomerDetails;
+import com.mps.think.setup.model.Order;
+import com.mps.think.setup.repo.AddOrderRepo;
+import com.mps.think.setup.repo.CustomerDetailsRepo;
+//import com.mps.think.setup.service.GlobalSearchService;
+import com.mps.think.setup.service.GlobalSearchService;
+
+@Service
+public class GlobalSearchServiceImpl implements GlobalSearchService {
+	
+	@Autowired
+	private AddOrderRepo orderRepo;
+	
+	@Autowired
+	private CustomerDetailsRepo customerRepo;
+
+	@Override
+	public Map<String, Object> getSearchResults(String keyword, Pageable customersPage) {
+		Map<String, Object> result = new HashMap<>();
+		Page<CustomerDetails> customerResults = customerRepo.getAllCustomerDetailsForSearchSingle(keyword, customersPage);
+//		Page<Order> orderResults = orderRepo.findOrdersBySearch(keyword, ordersPage);
+		result.put("customers", customerResults);
+//		result.put("orders", orderResults);
+		return result;
+	}
+
+
+}
