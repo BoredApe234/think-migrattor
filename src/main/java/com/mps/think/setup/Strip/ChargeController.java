44e5c4b022524ee4567f297d19d4d83bada5d4e7
Akshay-R <114207199+AkshayPathakAhom@users.noreply.github.com>
2023-02-21 09:06:28 +0530

    payment

---
+++ src/main/java/com/mps/think/setup/Strip/ChargeController.java
@@ -0,0 +1,38 @@
+package com.mps.think.setup.Strip;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.mps.think.setup.Strip.ChargeRequest.Currency;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+
+@Controller
+public class ChargeController {
+
+    @Autowired
+    StripeService paymentsService;
+
+    @PostMapping("/charge")
+    public String charge(ChargeRequest chargeRequest, Model model) throws StripeException {
+        chargeRequest.setDescription("Example charge");
+        chargeRequest.setCurrency(Currency.EUR);
+        Charge charge = paymentsService.charge(chargeRequest);
+        model.addAttribute("id", charge.getId());
+        model.addAttribute("status", charge.getStatus());
+        model.addAttribute("chargeId", charge.getId());
+        model.addAttribute("balance_transaction", charge.getBalanceTransaction());
+        System.out.println(chargeRequest.toString());
+        System.out.println(charge.toString());
+        return "result";
+    }
+
+    @ExceptionHandler(StripeException.class)
+    public String handleError(Model model, StripeException ex) {
+        model.addAttribute("error", ex.getMessage());
+        return "result";
+    }
+}
---
+++ src/main/java/com/mps/think/setup/Strip/ChargeRequest.java
@@ -0,0 +1,50 @@
+package com.mps.think.setup.Strip;
+public class ChargeRequest {
+
+    public enum Currency {
+        EUR, USD;
+    }
+    private String description;
+    private int amount; // cents
+    private Currency currency;
+    private String stripeEmail;
+    private String stripeToken;
+    public String getDescription() {
+        return description;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public Currency getCurrency() {
+        return currency;
+    }
+    public String getStripeEmail() {
+        return stripeEmail;
+    }
+    public String getStripeToken() {
+        return stripeToken;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public void setStripeEmail(String stripeEmail) {
+		this.stripeEmail = stripeEmail;
+	}
+	public void setStripeToken(String stripeToken) {
+		this.stripeToken = stripeToken;
+	}
+	@Override
+	public String toString() {
+		return "ChargeRequest [description=" + description + ", amount=" + amount + ", currency=" + currency
+				+ ", stripeEmail=" + stripeEmail + ", stripeToken=" + stripeToken + "]";
+	}
+    
+    
+    
+}
