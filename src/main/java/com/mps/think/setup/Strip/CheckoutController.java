cec2b36e47cc541b9a92405bdb3464417a3fa5c0
Akshay-R <114207199+AkshayPathakAhom@users.noreply.github.com>
2023-02-24 11:13:42 +0530

    strip

--- src/main/java/com/mps/think/setup/Strip/CheckoutController.java
+++ src/main/java/com/mps/think/setup/Strip/CheckoutController.java
@@ -1,20 +1,35 @@
 package com.mps.think.setup.Strip;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.mps.think.setup.vo.PublicKey;
+
+@RestController
 public class CheckoutController {
 
     @Value("${STRIPE_PUBLIC_KEY}")
     private String stripePublicKey;
 
-    @RequestMapping("/checkout")
-    public String checkout(Model model) {
-        model.addAttribute("amount", 50 * 100); // in cents
-        model.addAttribute("stripePublicKey", stripePublicKey);
-        model.addAttribute("currency", ChargeRequest.Currency.EUR);
-        return "checkout";
+//    @RequestMapping("/checkout")
+//    public String checkout(Model model) {
+//        model.addAttribute("amount", 50 * 100); // in cents
+//        model.addAttribute("stripePublicKey", stripePublicKey);
+//        model.addAttribute("currency", ChargeRequest.Currency.EUR);
+//        return "checkout";
+//    }
+    
+    // Get Public  key to start the Transition 1st API for Strip  call 
+    @GetMapping("/checkOutThePublicKey")
+    public  ResponseEntity<?> checkOutThePublicKey() {
+    	PublicKey pk= new PublicKey();
+    	pk.setStrippublick(stripePublicKey);
+        return ResponseEntity.ok(pk);
     }
+  
+    
 }
