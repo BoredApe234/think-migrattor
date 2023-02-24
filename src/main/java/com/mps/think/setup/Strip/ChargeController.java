ed23155018d5dbed19f1533bfb5db297c044d330
Akshay-R <114207199+AkshayPathakAhom@users.noreply.github.com>
2023-02-24 11:14:29 +0530

    un-use import

--- src/main/java/com/mps/think/setup/Strip/ChargeController.java
+++ src/main/java/com/mps/think/setup/Strip/ChargeController.java
@@ -2,12 +2,10 @@ package com.mps.think.setup.Strip;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mps.think.setup.Strip.ChargeRequest.Currency;
