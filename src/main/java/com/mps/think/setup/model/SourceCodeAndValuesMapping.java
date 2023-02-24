1ec9fd3d395d9f72e52361bb88f6cf58e84023c1
amitkr0099 <amit.kr@mpslimited.com>
2023-02-24 15:46:54 +0530

    added mapping value into source code api

---
+++ src/main/java/com/mps/think/setup/model/SourceCodeAndValuesMapping.java
@@ -0,0 +1,67 @@
+package com.mps.think.setup.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+@Table(name = "source_code_values_mapping")
+@Entity
+public class SourceCodeAndValuesMapping extends BaseEntity{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@OneToOne
+	@JoinColumn(name = "source_attribute_values_id",referencedColumnName="id")
+	private SourceAttributeValues sourceAttributeValues;
+	
+	@ManyToOne
+    @JoinColumn(name = "source_code_id",referencedColumnName="sourc_code_id")
+	@JsonBackReference
+	private SourceCode sourceCode;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public SourceAttributeValues getSourceAttributeValues() {
+		return sourceAttributeValues;
+	}
+
+	public void setSourceAttributeValues(SourceAttributeValues sourceAttributeValues) {
+		this.sourceAttributeValues = sourceAttributeValues;
+	}
+
+	public SourceCode getSourceCode() {
+		return sourceCode;
+	}
+
+	public void setSourceCode(SourceCode sourceCode) {
+		this.sourceCode = sourceCode;
+	}
+
+	@Override
+	public String toString() {
+		return "SourceCodeAndValuesMapping [id=" + id + ", sourceAttributeValues=" + sourceAttributeValues
+				+ ", sourceCode=" + sourceCode + "]";
+	}
+	
+	
+}
