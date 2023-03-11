a5a8c7ee4a6c40d45f38acf0c1db73038bd4031d
Samir Anand <Samir.Anand@macmill.com>
2023-03-11 10:45:19 +0530

    find by id

--- src/main/java/com/mps/think/setup/controller/KeyLineController.java
+++ src/main/java/com/mps/think/setup/controller/KeyLineController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,11 @@ public class KeyLineController {
 	@DeleteMapping("/deleteByLableKeyLineId")
 	public ResponseEntity<?> deleteByLableKeyLineId(@RequestBody Integer lableKeylineId) {
 		return ResponseEntity.ok(lableKeyLineServiceImpl.deleteByLableKeyLineId(lableKeylineId));
+	}
+	
+	@GetMapping("getAllLableKeyLineForPublisher/{publisherId}")
+	public ResponseEntity<?> getAllLableKeyLineForPublisher(@PathVariable("publisherId") Integer lableKeylineId) {
+		return ResponseEntity.ok(lableKeyLineServiceImpl.findAllLableKeyLineByPubId(lableKeylineId));
 	}
 
 }
