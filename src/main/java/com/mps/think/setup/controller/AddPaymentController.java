97f82073aa5b32c2c770735f53948c212afa4519
amitkr0099 <amit.kr@mpslimited.com>
2023-03-21 18:05:50 +0530

    created add payment api

---
+++ src/main/java/com/mps/think/setup/controller/AddPaymentController.java
@@ -0,0 +1,44 @@
+package com.mps.think.setup.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mps.think.setup.service.AddPaymentService;
+import com.mps.think.setup.vo.AddPaymentVO;
+import com.mps.think.setup.vo.AuxVariableVO;
+
+@RestController
+@CrossOrigin
+public class AddPaymentController {
+	
+	@Autowired
+	AddPaymentService addPaymentService;
+	
+
+	@PostMapping("/findAllPaymentForPublisher")
+	public ResponseEntity<?> findAllPaymentForPublisher(@RequestBody Integer pubId) {
+		return ResponseEntity.ok(addPaymentService.findAllPaymentForPublisher(pubId));
+	}
+
+	@PostMapping("/savePayment")
+	public ResponseEntity<?> savePayment(@RequestBody AddPaymentVO paymentVO) {
+		return ResponseEntity.ok(addPaymentService.savePayment(paymentVO));
+	}
+	
+	@PostMapping("/updatePayment")
+	public ResponseEntity<?> updatePayment(@RequestBody AddPaymentVO paymentVO) {
+		return ResponseEntity.ok(addPaymentService.updatePayment(paymentVO));
+	}
+	
+	@PostMapping("/findbyPaymentId")
+	public ResponseEntity<?> findbyPaymentId(@RequestBody Integer id) {
+		return ResponseEntity.ok(addPaymentService.findbyPaymentId(id));
+	}
+	
+
+}
