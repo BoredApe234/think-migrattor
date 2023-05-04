09cdfd02cba42c023ab1a5e8775326543123b4d5
Samir Anand <Samir.Anand@macmill.com>
2023-05-04 12:19:32 +0530

    multiple orderclass

---
+++ src/main/java/com/mps/think/setup/model/AddprocessmappingVO.java
@@ -0,0 +1,23 @@
+package com.mps.think.setup.model;
+
+public class AddprocessmappingVO {
+	
+	private Integer id;
+	private OrderClass orderClass;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public OrderClass getOrderClass() {
+		return orderClass;
+	}
+	public void setOrderClass(OrderClass orderClass) {
+		this.orderClass = orderClass;
+	}
+	
+	
+
+}
