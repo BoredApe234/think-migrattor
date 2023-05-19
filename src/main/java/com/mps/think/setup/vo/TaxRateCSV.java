550530ca9869072f17bb0c1906ede0120e90ec57
Akshay-R <114207199+AkshayPathakAhom@users.noreply.github.com>
2023-05-19 11:10:20 +0530

    tax csv

---
+++ src/main/java/com/mps/think/setup/vo/TaxRateCSV.java
@@ -0,0 +1,312 @@
+package com.mps.think.setup.vo;
+
+public class TaxRateCSV {
+
+	private String taxId;
+
+	private String registeredForTax;
+
+	private String forceTax;
+
+	private String taxType;
+
+	private String taxTypeDescription;
+
+	private String taxRateCategory;
+
+	private String taxRateCategoryDescription;
+
+	private String region;
+
+	private String country;
+
+	private String countryCode;
+
+	private String postal_code;
+
+	private String state;
+
+	private String stateCode;
+
+	private String city;
+
+	private String county;
+
+	private String streetName;
+
+	private String jurisdiction;
+
+	private String commodityCode;
+
+	private String commodityDescription;
+
+	private String avataxApplies;
+
+	private String basicRate;
+
+	private String commodityRate;
+
+	private String jurisdictionRate;
+
+	private String effectiveDateForBasicRate;
+
+	private String effectiveDateForCommodityRate;
+
+	private String effectiveDateForJurisdictionRate;
+
+	private String noTaxMessageBasicRate;
+
+	private String noTaxMessageCommodityRate;
+
+	private String noTaxMessageJurisdictionRate;
+
+	public String getTaxId() {
+		return taxId;
+	}
+
+	public void setTaxId(String taxId) {
+		this.taxId = taxId;
+	}
+
+	public String getRegisteredForTax() {
+		return registeredForTax;
+	}
+
+	public void setRegisteredForTax(String registeredForTax) {
+		this.registeredForTax = registeredForTax;
+	}
+
+	public String getForceTax() {
+		return forceTax;
+	}
+
+	public void setForceTax(String forceTax) {
+		this.forceTax = forceTax;
+	}
+
+	public String getTaxType() {
+		return taxType;
+	}
+
+	public void setTaxType(String taxType) {
+		this.taxType = taxType;
+	}
+
+	public String getTaxTypeDescription() {
+		return taxTypeDescription;
+	}
+
+	public void setTaxTypeDescription(String taxTypeDescription) {
+		this.taxTypeDescription = taxTypeDescription;
+	}
+
+	public String getTaxRateCategory() {
+		return taxRateCategory;
+	}
+
+	public void setTaxRateCategory(String taxRateCategory) {
+		this.taxRateCategory = taxRateCategory;
+	}
+
+	public String getTaxRateCategoryDescription() {
+		return taxRateCategoryDescription;
+	}
+
+	public void setTaxRateCategoryDescription(String taxRateCategoryDescription) {
+		this.taxRateCategoryDescription = taxRateCategoryDescription;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getPostal_code() {
+		return postal_code;
+	}
+
+	public void setPostal_code(String postal_code) {
+		this.postal_code = postal_code;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getJurisdiction() {
+		return jurisdiction;
+	}
+
+	public void setJurisdiction(String jurisdiction) {
+		this.jurisdiction = jurisdiction;
+	}
+
+	public String getCommodityCode() {
+		return commodityCode;
+	}
+
+	public void setCommodityCode(String commodityCode) {
+		this.commodityCode = commodityCode;
+	}
+
+	public String getCommodityDescription() {
+		return commodityDescription;
+	}
+
+	public void setCommodityDescription(String commodityDescription) {
+		this.commodityDescription = commodityDescription;
+	}
+
+	public String getAvataxApplies() {
+		return avataxApplies;
+	}
+
+	public void setAvataxApplies(String avataxApplies) {
+		this.avataxApplies = avataxApplies;
+	}
+
+	public String getBasicRate() {
+		return basicRate;
+	}
+
+	public void setBasicRate(String basicRate) {
+		this.basicRate = basicRate;
+	}
+
+	public String getCommodityRate() {
+		return commodityRate;
+	}
+
+	public void setCommodityRate(String commodityRate) {
+		this.commodityRate = commodityRate;
+	}
+
+	public String getJurisdictionRate() {
+		return jurisdictionRate;
+	}
+
+	public void setJurisdictionRate(String jurisdictionRate) {
+		this.jurisdictionRate = jurisdictionRate;
+	}
+
+	public String getEffectiveDateForBasicRate() {
+		return effectiveDateForBasicRate;
+	}
+
+	public void setEffectiveDateForBasicRate(String effectiveDateForBasicRate) {
+		this.effectiveDateForBasicRate = effectiveDateForBasicRate;
+	}
+
+	public String getEffectiveDateForCommodityRate() {
+		return effectiveDateForCommodityRate;
+	}
+
+	public void setEffectiveDateForCommodityRate(String effectiveDateForCommodityRate) {
+		this.effectiveDateForCommodityRate = effectiveDateForCommodityRate;
+	}
+
+	public String getEffectiveDateForJurisdictionRate() {
+		return effectiveDateForJurisdictionRate;
+	}
+
+	public void setEffectiveDateForJurisdictionRate(String effectiveDateForJurisdictionRate) {
+		this.effectiveDateForJurisdictionRate = effectiveDateForJurisdictionRate;
+	}
+
+	public String getNoTaxMessageBasicRate() {
+		return noTaxMessageBasicRate;
+	}
+
+	public void setNoTaxMessageBasicRate(String noTaxMessageBasicRate) {
+		this.noTaxMessageBasicRate = noTaxMessageBasicRate;
+	}
+
+	public String getNoTaxMessageCommodityRate() {
+		return noTaxMessageCommodityRate;
+	}
+
+	public void setNoTaxMessageCommodityRate(String noTaxMessageCommodityRate) {
+		this.noTaxMessageCommodityRate = noTaxMessageCommodityRate;
+	}
+
+	public String getNoTaxMessageJurisdictionRate() {
+		return noTaxMessageJurisdictionRate;
+	}
+
+	public void setNoTaxMessageJurisdictionRate(String noTaxMessageJurisdictionRate) {
+		this.noTaxMessageJurisdictionRate = noTaxMessageJurisdictionRate;
+	}
+
+	@Override
+	public String toString() {
+		return "TaxRateCSV [taxId=" + taxId + ", registeredForTax=" + registeredForTax + ", forceTax=" + forceTax
+				+ ", taxType=" + taxType + ", taxTypeDescription=" + taxTypeDescription + ", taxRateCategory="
+				+ taxRateCategory + ", taxRateCategoryDescription=" + taxRateCategoryDescription + ", region=" + region
+				+ ", country=" + country + ", countryCode=" + countryCode + ", postal_code=" + postal_code + ", state="
+				+ state + ", stateCode=" + stateCode + ", city=" + city + ", county=" + county + ", streetName="
+				+ streetName + ", jurisdiction=" + jurisdiction + ", commodityCode=" + commodityCode
+				+ ", commodityDescription=" + commodityDescription + ", avataxApplies=" + avataxApplies + ", basicRate="
+				+ basicRate + ", commodityRate=" + commodityRate + ", jurisdictionRate=" + jurisdictionRate
+				+ ", effectiveDateForBasicRate=" + effectiveDateForBasicRate + ", effectiveDateForCommodityRate="
+				+ effectiveDateForCommodityRate + ", effectiveDateForJurisdictionRate="
+				+ effectiveDateForJurisdictionRate + ", noTaxMessageBasicRate=" + noTaxMessageBasicRate
+				+ ", noTaxMessageCommodityRate=" + noTaxMessageCommodityRate + ", noTaxMessageJurisdictionRate="
+				+ noTaxMessageJurisdictionRate + "]";
+	}
+
+}
