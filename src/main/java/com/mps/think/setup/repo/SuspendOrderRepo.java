0eda286d88318c2e1033878079e0b6c3000c0aab
atul-mps <atul.dhadse@highwirepress.com>
2023-05-31 17:41:22 +0530

    query updation of suspension

--- src/main/java/com/mps/think/setup/repo/SuspendOrderRepo.java
+++ src/main/java/com/mps/think/setup/repo/SuspendOrderRepo.java
@@ -12,23 +12,31 @@ import com.mps.think.setup.model.SuspendOrder;
 @Repository
 public interface SuspendOrderRepo extends JpaRepository<SuspendOrder, Integer> {
 
-	@Query(value = "SELECT m.order_id, o.suspend_order_id FROM multi_line_item_order m LEFT JOIN orders_to_be_suspended o ON o.order_id = m.order_id \r\n"
-			+ "WHERE o.is_valid IS TRUE AND m.parent_order_id = :parentOrderId \r\n"
-			+ "UNION \r\n"
-			+ "SELECT m.order_id, o.suspend_order_id FROM multi_line_item_order m JOIN orders_to_be_suspended o ON o.order_id = m.order_id\r\n"
-			+ "WHERE m.parent_order_id = :parentOrderId AND o.is_valid IS TRUE AND (o.is_reinstated IS NULL || o.is_reinstated IS FALSE)",
-			countQuery = "SELECT COUNT(*) AS ROW_COUNT\r\n"
+	@Query(value = "SELECT m.order_id, o.suspend_order_id FROM orders_to_be_suspended o RIGHT JOIN multi_line_item_order m ON m.order_id = o.order_id WHERE m.parent_order_id = :parentOrderId AND\r\n"
+			+ "m.order_id NOT IN (SELECT o.order_id FROM orders_to_be_suspended o JOIN multi_line_item_order m ON m.order_id = o.order_id WHERE m.parent_order_id = :parentOrderId AND o.is_valid IS TRUE)\r\n"
+			+ "UNION\r\n"
+			+ "SELECT m.order_id, o.suspend_order_id FROM orders_to_be_suspended o JOIN multi_line_item_order m ON m.order_id = o.order_id WHERE m.parent_order_id = :parentOrderId AND o.is_valid IS TRUE\r\n",
+			countQuery = "SELECT COUNT(*) AS row_count\r\n"
 					+ "FROM (\r\n"
 					+ "    SELECT m.order_id, o.suspend_order_id\r\n"
-					+ "    FROM multi_line_item_order m\r\n"
-					+ "    LEFT JOIN orders_to_be_suspended o ON o.order_id = m.order_id\r\n"
-					+ "    WHERE o.is_valid IS TRUE AND m.parent_order_id = :parentOrderId\r\n"
-					+ "    UNION \r\n"
+					+ "    FROM orders_to_be_suspended o\r\n"
+					+ "    RIGHT JOIN multi_line_item_order m ON m.order_id = o.order_id\r\n"
+					+ "    WHERE m.parent_order_id = :parentOrderId\r\n"
+					+ "    AND m.order_id NOT IN (\r\n"
+					+ "        SELECT o.order_id\r\n"
+					+ "        FROM orders_to_be_suspended o\r\n"
+					+ "        JOIN multi_line_item_order m ON m.order_id = o.order_id\r\n"
+					+ "        WHERE m.parent_order_id = :parentOrderId\r\n"
+					+ "        AND o.is_valid IS TRUE\r\n"
+					+ "    )\r\n"
+					+ "    UNION\r\n"
 					+ "    SELECT m.order_id, o.suspend_order_id\r\n"
-					+ "    FROM multi_line_item_order m\r\n"
-					+ "    JOIN orders_to_be_suspended o ON o.order_id = m.order_id\r\n"
-					+ "    WHERE m.parent_order_id = :parentOrderId AND o.is_valid IS TRUE AND (o.is_reinstated IS NULL OR o.is_reinstated IS FALSE)\r\n"
-					+ ") AS subquery;",
+					+ "    FROM orders_to_be_suspended o\r\n"
+					+ "    JOIN multi_line_item_order m ON m.order_id = o.order_id\r\n"
+					+ "    WHERE m.parent_order_id = :parentOrderId\r\n"
+					+ "    AND o.is_valid IS TRUE\r\n"
+					+ ") AS subquery;\r\n"
+					+ "",
 			nativeQuery = true)
 	Page<Object[]> findAllOrdersByIdWithSuspensionDet(@Param("parentOrderId") Integer parentOrderId, Pageable page);
 
