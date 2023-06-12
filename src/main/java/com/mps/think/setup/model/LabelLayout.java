27af6c2e7014bc98f5ca18c54c8b72d1a713b3e5
Samir Anand <Samir.Anand@macmill.com>
2023-06-12 11:05:28 +0530

    lists

--- src/main/java/com/mps/think/setup/model/LabelLayout.java
+++ src/main/java/com/mps/think/setup/model/LabelLayout.java
@@ -53,8 +53,24 @@ public class LabelLayout extends BaseEntity {
 	private String display;
 	
 	@ElementCollection
-	@Column(name = "column_name")
-	private List<String> column_name;
+	@Column(name = "row1")
+	private List<String> row1;
+	
+	@ElementCollection
+	@Column(name = "row2")
+	private List<String> row2;
+	
+	@ElementCollection
+	@Column(name = "row3")
+	private List<String> row3;
+	
+	@ElementCollection
+	@Column(name = "row4")
+	private List<String> row4;
+	
+	@ElementCollection
+	@Column(name = "row5")
+	private List<String> row5;
 
 	public Integer getId() {
 		return id;
@@ -120,19 +136,52 @@ public class LabelLayout extends BaseEntity {
 		this.display = display;
 	}
 
-	public List<String> getColumn_name() {
-		return column_name;
+	public List<String> getRow1() {
+		return row1;
 	}
 
-	public void setColumn_name(List<String> column_name) {
-		this.column_name = column_name;
+	public void setRow1(List<String> row1) {
+		this.row1 = row1;
+	}
+
+	public List<String> getRow2() {
+		return row2;
+	}
+
+	public void setRow2(List<String> row2) {
+		this.row2 = row2;
+	}
+
+	public List<String> getRow3() {
+		return row3;
+	}
+
+	public void setRow3(List<String> row3) {
+		this.row3 = row3;
+	}
+
+	public List<String> getRow4() {
+		return row4;
+	}
+
+	public void setRow4(List<String> row4) {
+		this.row4 = row4;
+	}
+
+	public List<String> getRow5() {
+		return row5;
+	}
+
+	public void setRow5(List<String> row5) {
+		this.row5 = row5;
 	}
 
 	@Override
 	public String toString() {
 		return "LabelLayout [id=" + id + ", pubId=" + pubId + ", lableformat=" + lableformat + ", lablegroup="
 				+ lablegroup + ", description=" + description + ", suppressed=" + suppressed + ", truncated="
-				+ truncated + ", display=" + display + ", column_name=" + column_name + "]";
+				+ truncated + ", display=" + display + ", row1=" + row1 + ", row2=" + row2 + ", row3=" + row3
+				+ ", row4=" + row4 + ", row5=" + row5 + "]";
 	}
 
 	
--- src/main/java/com/mps/think/setup/vo/LabelLayoutVO.java
+++ src/main/java/com/mps/think/setup/vo/LabelLayoutVO.java
@@ -16,7 +16,11 @@ public class LabelLayoutVO {
 	private Boolean truncated;
 //	private CustomerDetailsVO customerdetails;
 	private String display;
-	private List<String> column_name;
+    private List<String> row1;
+	private List<String> row2;
+	private List<String> row3;
+	private List<String> row4;
+	private List<String> row5;
 	public Integer getId() {
 		return id;
 	}
@@ -65,19 +69,43 @@ public class LabelLayoutVO {
 	public void setDisplay(String display) {
 		this.display = display;
 	}
-	public List<String> getColumn_name() {
-		return column_name;
+	public List<String> getRow1() {
+		return row1;
 	}
-	public void setColumn_name(List<String> column_name) {
-		this.column_name = column_name;
+	public void setRow1(List<String> row1) {
+		this.row1 = row1;
+	}
+	public List<String> getRow2() {
+		return row2;
+	}
+	public void setRow2(List<String> row2) {
+		this.row2 = row2;
+	}
+	public List<String> getRow3() {
+		return row3;
+	}
+	public void setRow3(List<String> row3) {
+		this.row3 = row3;
+	}
+	public List<String> getRow4() {
+		return row4;
+	}
+	public void setRow4(List<String> row4) {
+		this.row4 = row4;
+	}
+	public List<String> getRow5() {
+		return row5;
+	}
+	public void setRow5(List<String> row5) {
+		this.row5 = row5;
 	}
 	@Override
 	public String toString() {
 		return "LabelLayoutVO [id=" + id + ", pubId=" + pubId + ", lableformat=" + lableformat + ", lablegroup="
 				+ lablegroup + ", description=" + description + ", suppressed=" + suppressed + ", truncated="
-				+ truncated + ", display=" + display + ", column_name=" + column_name + "]";
+				+ truncated + ", display=" + display + ", row1=" + row1 + ", row2=" + row2 + ", row3=" + row3
+				+ ", row4=" + row4 + ", row5=" + row5 + "]";
 	}
-	
 	
 	
 	
